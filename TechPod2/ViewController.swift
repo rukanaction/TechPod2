34136b3610aec012bb8200338bf399efc72a8082
rukanaction <ruka.34ever57@gmail.com>
2021-02-11 23:57:18 +0900

    Initial Commit

---
+++ TechPod2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechPod2
+//
+//  Created by Ruka Saito on 2021/02/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
